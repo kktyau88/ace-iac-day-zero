d6d59a28a4a15c988903e706b0de98b7a1fa6f3b
kktyau88 <101502459+kktyau88@users.noreply.github.com>
2022-06-14 22:54:41 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -18,7 +18,8 @@ variable "aws_account_name" {
 }
 
 variable "aws_transit_instance_size" {
-  default = "t2.micro"
+  #default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "aws_transit1_region" {
@@ -34,7 +35,8 @@ variable "aws_transit1_cidr" {
 }
 
 variable "aws_spoke_instance_size" {
-  default = "t2.micro"
+  #default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "aws_spoke1_region" {
